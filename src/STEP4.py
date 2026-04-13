81d3bac7d3a8c7153df5e8258333052376d2aa93
Eve Delegue <eve.delegue@ens-paris-saclay.fr>
2026-04-13 18:31:59 +0200

    Step4 fix

--- src/STEP4.py
+++ src/STEP4.py
@@ -4,14 +4,11 @@ import yaml
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-from pathlib import Path
 import argparse
 import pandas as pd
 
-from tiatoolbox.models.engine.nucleus_instance_segmentor import NucleusInstanceSegmentor
-from utils.utils_nucleus import get_contours_2, vectorize,pinv, getHstain, segmentNucleus, computeFeatures
+from utils.utils_nucleus import computeFeaturesArea, getNucleusFeaturesArea
 from utils.PGA import PGA
-import joblib
 
 
 
@@ -19,7 +16,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--slide_name", type=str)
+    parser.add_argument("--slide_name", type=str,default='data/patches/66A_PB')
     parser.add_argument("--verbose",type=bool,default=True)
     args = parser.parse_args()
     return args
@@ -30,36 +27,44 @@ def main():
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
 
+    # pga params
     hemato_1 = config["staining"]["hemato_1"]
     eosin = config["staining"]["eosin"]
     safran = config["staining"]["safran"]
     hemato_2 = config["staining"]["hemato_2"]
 
-    vis_scale = config["patching"]["vis_scale"]
-    tumor_checkpoints = config["paths"]["pth_to_tumor_ckpts"]
-    patches_path = config["paths"]["pth_to_patches_bis"]
-    save_dir = config["paths"]["pth_to_nuc_dats"]
-
-    path_to_verbose = config["paths"]["pth_to_nuc_viz"]
-
-
     Lambda = config["staining"]["lambda"] 
     poids = tuple(config["staining"]["poids"]) 
+    kernel_size = config["nucleus_model"]["kernel_size"]
+
+    vis_scale = config["patching"]["vis_scale"]
+    # paths
+    tumor_checkpoints = config["paths"]["pth_to_tumor_ckpts"]
+    patches_path = config["paths"]["pth_to_patches"]
+    coords_checkpoints = config["paths"]["pth_to_coords"]
+    save_dir = config["paths"]["pth_to_nuc_ckpts"]
+
+    path_to_verbose = config["paths"]["pth_to_nuc_viz"]
 
     # parse arguments
     args = parse_arguments()
     verbose = args.verbose
     slide_name = args.slide_name.split(os.path.sep)[-1] #ex : 93A_PB
 
+    hospital = slide_name.split('_')[-1] 
+    mpp_dict = config["patching"]["mpp_dict"]
+    if hospital not in mpp_dict.keys():
+        raise KeyError(f"no resolution defined for this hospital : {hospital}. Check that the hospital's name in the config file is the same as in the data folder.")
+    mpp = mpp_dict[hospital]
+    ref_mpp = mpp_dict[config["patching"]["ref_hospital"]]
 
-
-    # load checkpoints
+    # load tumor checkpoints
     chkpt_tumor = torch.load(f"{tumor_checkpoints}/{slide_name}_preds_probas_checkpoint.pt",weights_only=False)
+    # load coordinates
     chkpt_coords = torch.load(
-        f"checkpoints/coords_checkpoints/{slide_name}_coords_checkpoint.pt",weights_only=False
+        f"{coords_checkpoints}/{slide_name}_coords_checkpoint.pt",weights_only=False
     )
     
-
     coords_x, coords_y = [], []
     for patch in os.listdir(f"{patches_path}/{slide_name}"):
         _, _, x, _, y = patch[:-4].split("_")
@@ -69,53 +74,11 @@ def main():
     # init pga algorithm
     Wgt = -np.log(np.array([hemato_1, eosin, safran, hemato_2]).T / 255)
     pga = PGA(Wgt, device=device)
-    
-    
-    os.makedirs(f"data/patches_He/{slide_name}",exist_ok=True) # ex : data/patches_He/93A_PB
-    # init model
-    model = NucleusInstanceSegmentor(
-        pretrained_model="hovernet_fast-pannuke", batch_size=256
-    )
 
     # load tumor slides
     y_har_preds = chkpt_tumor["har_mean_preds"]
     y_harmonic = y_har_preds.numpy()
     
-    # stain separation
-    images = []
-    for j in tqdm(range(len(y_har_preds)), desc="load images and gen Hematoxylin"):
-        x, y, p = coords_x[j], coords_y[j], y_harmonic[j]
-        if p != 0: # si pejorative
-            try:
-                # read img
-                patch = f"patch_x_{x}_patch_y_{y}.jpg"
-                im = plt.imread(f"{patches_path}{slide_name}/{patch}") # ex : data/patches/93A_PB/patch_x_33956_y_93057.jpg
-                # stain separation
-                V = vectorize(im)
-                N, M, _ = im.shape
-                H0 = np.maximum((pinv(Wgt) @ V), 0)
-
-                im_He = getHstain(V, Wgt, H0, Lambda, pga, poids, n=im.shape[0])
-                plt.imsave(
-                    f"data/patches_He/{slide_name}/{patch.replace('.', '_He.')}", im_He
-                )
-                images.append(
-                    f"data/patches_He/{slide_name}/{patch.replace('.', '_He.')}"
-                )
-            except Exception as e:
-                print(f"Error in {x} {y}: {e}")
-                continue
-    
-    # segmentation on the He patch
-    images.sort()
-    tile_output = model.predict(
-        images,
-        mode="tile",
-        save_dir=save_dir + f"/{slide_name}",
-        device=device,
-        crash_on_exception=True,
-    )
-
     all_x, all_y, median_areas, mean_areas, nucleocytos, densities, anisos, median_vars = (
         [],
         [],
@@ -126,23 +89,20 @@ def main():
         [],
         [],
     )
-    for i in tqdm(range(len(tile_output))):
-        # compute nucleus features
-        try:
-            # read pred
-            tile_preds = joblib.load(f"{tile_output[i][1]}.dat")
-            # read patch
-            im = plt.imread(tile_output[i][0])
-            contours = get_contours_2(inst_dict=tile_preds)
-            final_im = segmentNucleus(im, contours) # create a 3 colors image, white, pink, blue
-            density, mean_area, median_area, aniso, _, nucyto_idx = computeFeatures(
-                contours, final_im
-            ) 
-            if i%1000==1 and verbose:
-                plt.imsave(os.path.join(path_to_verbose,'He_'+tile_output[i][0]),im)
-                plt.imsave(os.path.join(path_to_verbose,'colored_'+tile_output[i][0]),final_im)
-                
-            _, _, x, _, y, _ = str(tile_output[i][0]).split("/")[-1].split("_")
+    # stain separation
+
+    sub_verbose = verbose
+    for j in tqdm(range(20), desc="load images and detect nuclei features"):#len(y_har_preds)), desc="load images and gen Hematoxylin"):
+        x, y, p = coords_x[j], coords_y[j], y_harmonic[j]
+        if p != 0: # si tumorale
+            # read img
+            patch = f"patch_x_{x}_y_{y}.jpg"
+            im = plt.imread(f"{patches_path}{slide_name}/{patch}") # ex : data/patches/93A_PB/patch_x_33956_y_93057.jpg
+            # stain separation and nuclei detection
+            areas,final_im,density = getNucleusFeaturesArea(im, Wgt, Lambda, pga,  poids, kernel_size, sub_verbose,path_to_verbose+'/'+str(j)+'_'+str(slide_name),mpp,ref_mpp) # create a 3 colors image, white, pink, blue
+            density, mean_area, median_area, aniso, _, nucyto_idx = computeFeaturesArea(areas, final_im,density)
+            sub_verbose = False
+            
             all_x.append(int(x))
             all_y.append(int(y))
             densities.append(density)
@@ -150,9 +110,7 @@ def main():
             median_areas.append(median_area)
             anisos.append(aniso)
             nucleocytos.append(nucyto_idx)
-        except Exception as e:
-            print(f"Error in {str(tile_output[i][0])}: {e}")
-            continue
+            
 
     [x_start, y_start, _, _] = chkpt_coords["xy_start_end"]
     coords_x = np.array(coords_x) * vis_scale - x_start
@@ -170,7 +128,7 @@ def main():
         }
     )
     df_features.to_csv(
-        f"checkpoints/nucleus_checkpoints/{slide_name}_nucleus_features.csv", index=False
+        f"{save_dir}/{slide_name}_nucleus_features.csv", index=False
     )
 
 
