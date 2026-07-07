75cf103a6e3591d08cedc2232f07e1a4595d368f
Eve Delegue <eve.delegue@ens-paris-saclay.fr>
2026-07-07 13:17:32 +0200

    get patchs from coords

--- src/expert_feature_extraction.py
+++ src/expert_feature_extraction.py
@@ -1,15 +1,5 @@
-from utils.patch_generation import mask_tissue
-
-image_path = "/home/eve/personal/CCK_survival_prediction/raw_data/3eme_batch/CKDG082.svs"
-#### Patch classification (Pej vs non pej vs sain)
-
-## extraction des patchs
-
-# 1 masque de tissus
-
-mask, slide = mask_tissue(image_path,verbose=True,verbose_path="brouillons/visuals_biopsy_sat4",n_threshold=4,chanel='saturation')
-
-# 2 decoupe de coordonnées des patchs
+from utils.patch_generation import mask_tissue, get_patch_coords
+import os
 
 image_path = "data/WSIs/BJ/Patient_224/224B.svs"
 #### Patch classification (Pej vs non pej vs sain)
@@ -21,3 +11,32 @@ image_path = "data/WSIs/BJ/Patient_224/224B.svs"
 mask, slide = mask_tissue(image_path,verbose=True,verbose_path="brouillons/visuals_biopsy_sat4",n_threshold=4,chanel='saturation')
 
 # 2 decoupe de coordonnées des patchs
+
+get_patch_coords(slide,mask,size=(287,287),step=(1,1),verbose=True,verbose_path=os.path.join("brouillons/visuals",os.path.basename(image_path).split('.')[0]))
+    
+
+image_path = "data/WSIs/HM/Patient_124/124A.ndpi"
+#### Patch classification (Pej vs non pej vs sain)
+
+## extraction des patchs
+
+# 1 masque de tissus
+
+mask, slide = mask_tissue(image_path,verbose=True,verbose_path="brouillons/visuals_biopsy_sat4",n_threshold=4,chanel='saturation')
+
+# 2 decoupe de coordonnées des patchs
+
+get_patch_coords(slide,mask,size=(287,287),step=(1,1),verbose=True,verbose_path=os.path.join("brouillons/visuals",os.path.basename(image_path).split('.')[0]))
+
+image_path = "data/WSIs/PB/Patient_73/73B.mrxs"
+#### Patch classification (Pej vs non pej vs sain)
+
+## extraction des patchs
+
+# 1 masque de tissus
+
+mask, slide = mask_tissue(image_path,verbose=True,verbose_path="brouillons/visuals_biopsy_sat4",n_threshold=4,chanel='saturation')
+
+# 2 decoupe de coordonnées des patchs
+
+get_patch_coords(slide,mask,size=(287,287),step=(1,1),verbose=True,verbose_path=os.path.join("brouillons/visuals",os.path.basename(image_path).split('.')[0]))
--- utils/patch_generation.py
+++ utils/patch_generation.py
@@ -5,8 +5,9 @@ import os
 import cv2
 from skimage.filters import threshold_multiotsu
 import warnings
+import itertools
 
-def get_patch_coords(slide:op.OpenSlide,mask:np.ndarray,size:tuple[int,int],step:tuple[int,int],mpp:int,verbose:bool=False,verbose_path:str="brouillons/visuals")->list[tuple]:
+def get_patch_coords(slide:op.OpenSlide,mask:np.ndarray,size:tuple[int,int]=(287,287),step:tuple[int,int]=(1,1),verbose:bool=False,verbose_path:str="brouillons/visuals")->list[tuple]:
     """create a list of patch coordinates in the slide inside the mask, for the desired size and step in the desired mpp. The coordinates are not necessarily of the right size, but when rescaled to the right mpp they will.
     
     :param slide: input tile  
@@ -17,14 +18,45 @@ def get_patch_coords(slide:op.OpenSlide,mask:np.ndarray,size:tuple[int,int],step
     :type verbose: bool
     :param mask: downscaled mask of tissue segmentation
     :type mask: ndarray
-    :param size: desired patch size 
+    :param size: desired patch size in micron
     :type size: tuple[int,int]
-    :param step: desired step between patchs 
+    :param step: desired step between patchs in proportion
     :type step: tuple[int,int]
     :param mpp: desired resolution in mpp (0.5 mpp for 20x and 0.25 mpp for 40x)
     :type mpp: int
     """
-    # 
+    # get mpp
+    mpp = float(slide.properties[op.PROPERTY_NAME_MPP_X])
+    # get patch size at level 0
+    patch_size_p = size[0]//mpp,size[1]//mpp
+    patch_size_p=tuple(int(i) for i in patch_size_p)
+    # get level 0 dimensions in pixels
+    dim_0 = slide.dimensions
+    # get patch coordinates for the whole image
+    full_coordinates = np.arange(0,dim_0[0],patch_size_p[0]*step[0],), np.arange(0,dim_0[1],patch_size_p[1]*step[1])
+    full_coords = list(itertools.product(full_coordinates[0],full_coordinates[1]))
+    # get downsample factor from thumbnail
+    dezoom_level = slide.level_downsamples[-1]
+    # filter patchs in mask
+    filtered_coords = [(int(y),int(x)) for x,y in full_coords if mask[int(y//dezoom_level),int(x//dezoom_level)]]
+    if verbose:
+        thumbnail = np.asarray(slide.get_thumbnail(size=slide.level_dimensions[-1])).copy()
+        for x,y in filtered_coords:
+            thumbnail[int(x//dezoom_level)-1:int(x//dezoom_level)+1,int(y//dezoom_level)-1:int(y//dezoom_level)+1] = (0,0,255)
+        os.makedirs(verbose_path,exist_ok=True)
+        # visualize thumbnail with patchs
+        plt.imsave(os.path.join(verbose_path,"thumbnail.png"),thumbnail)
+        # visualize some patchs at random
+
+        items_coords_id = np.random.choice(len(filtered_coords),4)
+        items_coords = np.array(filtered_coords)[items_coords_id]
+        for center in items_coords:
+            x = center[0]-patch_size_p[0]//2
+            y = center[1]-patch_size_p[1]//2 
+            patch = slide.read_region((y,x),0,patch_size_p)
+            plt.imsave(os.path.join(verbose_path,f"{x}_{y}_patch.png"),patch)
+
+
 
 
 def mask_tissue(tile_path:str,verbose:bool=False,verbose_path:str="brouillons/visuals",n_threshold:int=4,chanel:str='saturation')->np.ndarray:
