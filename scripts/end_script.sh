fb40c8617294eaed19a7ddaf4b9c623e35dce950
Eve Delegue <eve.delegue@ens-paris-saclay.fr>
2026-09-01 12:17:12 +0200

    big cleaning, removing code that is not useful for V2 branch

--- scripts/end_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python src/STEP0_create_directories.py
-
-python src/STEP5.py
-python src/STEP6.py
-python src/STEP7.py
-python src/STEP8.py
--- src/STEP0_create_directories.py
+++
@@ -1,3 +0,0 @@
-from utils.init import create_directories
-
-create_directories()
