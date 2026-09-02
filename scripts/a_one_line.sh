88a7e0063cab3bbaa3c0ffc069f463ab8cbf3f28
Eve Delegue <eve.delegue@ens-paris-saclay.fr>
2026-09-02 10:45:59 +0200

    renamed scripts

--- scripts/a_one_line.sh
+++
@@ -1,6 +0,0 @@
-folder_name="/mnt/backup/aziz_chaari/asadraoui_data_backups/backup_HM/Patient_160"
-dest_folder="data/WSIs/HM"
-mkdir $dest_folder
-cp -r $folder_name $dest_folder
-echo "process"
-rm -r $dest_folder
